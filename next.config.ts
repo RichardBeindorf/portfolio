c960e7567be1b17e3ab01fc4fe1bac765a4af51f
Richard Beindorf <richard_beindorf@web.de>
2025-05-05 16:25:17 +0200

    did some setup for three js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const withTM = require('next-transpile-modules')(['three'])
+module.exports = withTM()
+
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ['three'],
 };
+
 
 export default nextConfig;
