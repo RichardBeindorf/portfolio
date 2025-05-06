76927330f6cfc5cb378d3cf730b8e8d0ff87f9e3
Richard Beindorf <richard_beindorf@web.de>
2025-05-06 15:00:27 +0200

    dynamic lines progress and working on canvas size first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 import { permanentMarker } from "../styles/font";
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import ScribbleFigure from "@/components/scribbleFigure";
 import styled from "styled-components";
 import { Canvas } from "@react-three/fiber";
 import ThreeLine from "@/components/threeLine";
-
-type Vec2 = { x: number; y: number };
 
 const IntroHeader = styled.h1`
 	position: absolute;
@@ -22,32 +20,37 @@ const WelcomeMain = styled.main`
 	justify-content: center;
 	align-items: center;
 `;
+const CanvasLayer = styled(Canvas)`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 1000px;
+    overflow: hidden;
+`;
 
 export default function Home() {
-	const [_document, set_document] = React.useState(null);
+	// const [_document, set_document] = React.useState(null);
 	// const lastPos = useRef<number[] | null>(null);
-	const lineData = useRef([0,0]);
+	const [lines, setLines] = useState<number[][]>([]);
 
-	React.useEffect(() => {
-		set_document(document);
-		// points.push(1);
-	}, []);
+	// React.useEffect(() => {
+	// 	set_document(document);
+	// }, []);
 
 	const cursor: {x: number | null, y: number | null} = {
 		x: null,
 		y: null,
 	};
 
-	const points: number[] = [];
-
-	const canvasHeight = 900;
-	const canvasWidth = _document?.body.clientWidth;
+	// const canvasHeight = 900;
+	// const canvasWidth = _document?.body.clientWidth;
 
 	function mouseMove(e) {
 		cursor.x = e.clientX;
 		cursor.y = e.clientY +11;
-		lineData.current.push(e.clientX, e.clientY + 11);
-		console.log(lineData);
+		setLines(prev => [...prev, [e.clientX, e.clientY + 11]]);
+		console.log(lines);
 	}
 
 	function touchHandler(e) {
@@ -63,9 +66,9 @@ export default function Home() {
 			>
 				<IntroHeader style={permanentMarker.style} >I`m Richard <br/> a Creative Developer <br/> based in Hamburg</IntroHeader>
 				<ScribbleFigure/>
-				<Canvas>
-					<ThreeLine points={lineData}/>
-				</Canvas>
+				<CanvasLayer>
+					<ThreeLine points={lines}/>
+				</CanvasLayer>
 			</WelcomeMain>
 	);
 }
--- src/components/threeLine.tsx
+++ src/components/threeLine.tsx
@@ -1,32 +1,62 @@
-import { Line, QuadraticBezierLine } from "@react-three/drei";
-import { useEffect } from "react";
+"use client"
 
-export default function ThreeLine (points: number[]) {
+import { QuadraticBezierLine } from "@react-three/drei";
+// import { useEffect, useRef } from "react";
 
-    // const startPoints = points
+type coords = [number, number, number];
 
-    useEffect(() => {
+export default function ThreeLine ({ points }: { points: number[][]}) {
 
-    }, [])
+    // useEffect(() => {
+        // for(let i = 0; i < points.length; i){
+        //     if(points[i]){
+        //         const startCoords: coords = [points[i][0], points[i][1], 0];
+        //         const endCoords: coords = [points[i+1][0], points[i+1][1], 0];
+            
+        //     lines.current = (
+        //     <QuadraticBezierLine 
+        //         start={startCoords}               // Starting point, can be an array or a vec3
+        //         end={endCoords}               // Ending point, can be an array or a vec3
+        //         // mid={[5, 0, 1]}                 // Optional control point, can be an array or a vec3
+        //         color="#F24150"                   // Default
+        //         lineWidth={2}                   // In pixels (default)
+        //         dashed={false}     
+        //     />
+        // )
+        //     }
+        // };
+    // }, [points])
 
-    return (
-        <>
-            <mesh>
+    return  <mesh>
                 {/* <Line 
-                    points={[[0,0],[2,2]]}
-                    color={"black"}
-                    lineWidth={2}
-                    dashed={false} 
-                /> */}
-                <QuadraticBezierLine 
-                    start={[1, 0, 0]}               // Starting point, can be an array or a vec3
-                    end={[7, 1, 0]}               // Ending point, can be an array or a vec3
-                    // mid={[5, 0, 1]}                 // Optional control point, can be an array or a vec3
-                    color="#F24150"                   // Default
-                    lineWidth={2}                   // In pixels (default)
-                    dashed={false}     
-                />
+                        points={[[0,0],[2,2]]}
+                        color={"black"}
+                        lineWidth={2}
+                        dashed={false} 
+                    /> */}
+                {
+                points.map((p,i,a)=>{
+                    const startCoord: coords= [p[0], p[1],0];
+                    let endCoord: coords;
+
+                    if(a[i+1]){
+                        endCoord = [a[i+1][0], a[i+1][1], 0];
+                    }
+
+                    
+                    if(endCoord){
+                        console.log("start and end coord", startCoord, endCoord)
+                        return(
+                        <QuadraticBezierLine 
+                            key={i}
+                            start={startCoord}               // Starting point, can be an array or a vec3
+                            end={endCoord}               // Ending point, can be an array or a vec3
+                            // mid={[5, 0, 1]}                 // Optional control point, can be an array or a vec3
+                            color="#F24150"                   // Default
+                            lineWidth={2}                   // In pixels (default)
+                            dashed={false}     
+                        />)
+                    }
+                })}
             </mesh>
-        </>
-    )
 }
